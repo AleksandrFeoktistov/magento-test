7a5a3d3253be82b3e4dd5f6e5d641f94263158db
aleksandrfeoktistov <aleksandr_al58@mail.ru>
2018-12-25 13:14:27 +0200

    Observer getUrl
    remove spaces
    
    remove var_dump

--- app/code/Vendor/GetUrl/Observer/GetUrl.php
+++ app/code/Vendor/GetUrl/Observer/GetUrl.php
@@ -9,17 +9,15 @@ class GetUrl implements ObserverInterface
   private $_logger;
   public function __construct(\Psr\Log\LoggerInterface $logger)
   {
-    $this -> _logger = $logger;
+    $this->_logger = $logger;
     // Observer initialization code...
     // You can use dependency injection to get any class this observer may need.
   }
 
   public function execute(\Magento\Framework\Event\Observer $observer)
   {
-    $url = $observer->getEvent()-> getRequest() -> getPathinfo();
-    var_dump($url);
-    //var_dump($Url)
-    $this-> _logger ->addInfo($url);
+    $url = $observer->getEvent()->getRequest()->getPathinfo();
+    $this->_logger->addInfo($url);
     // Observer execution code..
   }
 }
